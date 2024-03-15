9746a80f64d9019d0be21bed57187df6c9235fc0
Jack crumpleys <jackcrumpleys@gmail.com>
2024-03-16 08:43:16 +1300

    Use H and L to navigate between tabs

--- lua/base/4-mappings.lua
+++ lua/base/4-mappings.lua
@@ -297,13 +297,13 @@ maps.n["<leader>ba"] = {
   function() vim.cmd "wa" end,
   desc = "Write all changed buffers",
 }
-maps.n["]b"] = {
+maps.n["L"] = {
   function()
     require("heirline-components.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
   end,
   desc = "Next buffer",
 }
-maps.n["[b"] = {
+maps.n["H"] = {
   function()
     require("heirline-components.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
   end,
